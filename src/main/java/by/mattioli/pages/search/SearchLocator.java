4e4a7062ab46db634458dfe49e191b30b30eec94
Irina <iragovyadina@mail.ru>
2026-02-03 12:16:44 +0300

    refactor LoginTest, SearchTest

--- src/main/java/by/mattioli/pages/search/SearchLocator.java
+++ src/main/java/by/mattioli/pages/search/SearchLocator.java
@@ -3,9 +3,9 @@ package by.mattioli.pages.search;
 import org.openqa.selenium.By;
 
 public class SearchLocator {
-    public static final By SEARCH_BAR_TITLE = By.xpath("//div[@class='form-group']//input[@id='search_input_popup' and @placeholder='Поиск товара']");
-    public static final By BUTTON_CLICK_SEARCH_BAR = By.xpath("//div[@class='header-search__input']//input[@id='search_input']");
-    public static final By SEARCH_BAR_FOUND_TEXT = By.xpath("//input[@id='search_input_popup' and @class='form-control' and @type='text']");
+    public static final By SEARCH_BAR_TITLE = By.xpath("//input[@id='search_input_popup']");
+    public static final By BUTTON_CLICK_SEARCH_BAR = By.xpath("//input[@id='search_input']");
+    public static final By SEARCH_BAR_FOUND_TEXT = By.xpath("//input[@id='search_input_popup']");
     public static final By SEARCH_BAR_RESULT_TEXT = By.xpath("//div[@class='search-results__total']/span");
 }
 
