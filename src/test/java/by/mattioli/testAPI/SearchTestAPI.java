c456b3fc1a22baece8c1cc53f4733ef446c7c4f0
Irina <iragovyadina@mail.ru>
2026-02-07 14:20:28 +0300

    refactor coda, add resources

--- src/test/java/by/mattioli/testAPI/SearchTestAPI.java
+++ src/test/java/by/mattioli/testAPI/SearchTestAPI.java
@@ -12,7 +12,7 @@ public class SearchTestAPI {
     private static final Logger logger = LogManager.getLogger();
 
     @Test
-    public void testSearch() {
+    public void testSearchBar() {
         SearchService searchService = new SearchService();
         logger.info("Выполняется поиск по запросу:'015'");
         searchService.doRequest("015");
